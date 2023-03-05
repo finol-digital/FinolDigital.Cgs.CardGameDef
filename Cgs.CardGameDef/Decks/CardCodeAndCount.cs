e78fbea825c27e048d6685dfcf7498609014d8bd
David Finol <davidmfinol@gmail.com>
2023-03-05 02:30:38 -0600

    Add implementation

---
+++ Cgs.CardGameDef/Decks/CardCodeAndCount.cs
@@ -0,0 +1,10 @@
+// https://github.com/RiotGames/LoRDeckCodes/blob/main/LoRDeckCodes/CardCodeAndCount.cs
+
+namespace Cgs.CardGameDef.Decks
+{
+    public class CardCodeAndCount
+    {
+        public string CardCode { get; set; }
+        public int Count { get; set; }
+    }
+}
